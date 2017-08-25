5baa9af94a18f0057345ce4facfe52a63bd50ba3
JiahuiRuan <jr58@rice.edu>
2017-08-25 01:41:13 -0500

    learn variables

---
+++ Var/Debug/src/Var.d
@@ -0,0 +1,1 @@
+src/Var.o: ../src/Var.cpp
---
+++ Var/src/Var.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Var.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int numOfCats = 10;
+	cout << "!!!Hello World!!!" << numOfCats << endl; // prints !!!Hello World!!!
+	return 0;
+}
